fb0865c880ded0184b8ca147a7b1c362c779627c
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-05-08 00:50:11 +0200

    Added a test for the issue fixed by #1 (notifying sink about termination)

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ const takeUntil = sSrc => src => (start, sink) => {
       sourceTalkback = d;
 
       sSrc(0, (st, sd) => {
+        if (done) return;
+
         if (st === 0) {
           sTalkback = sd;
           sTalkback(1);
-        } else if (st === 1) {
-          sourceTalkback(2);
-          sTalkback(2);
-          inited && sink(2);
-          done = true;
+          return
         }
+        done = true;
+        sourceTalkback(2);
+        sTalkback(2);
+        inited && sink(2);
       });
 
+      inited = true;
+
       sink(0, (st, sd) => {
-        inited = true;
         st === 2 && sTalkback(2);
         !done && sourceTalkback(st, sd);
       });
